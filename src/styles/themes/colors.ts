99c12c4e76c3616a31cf29655f3b74f24b4a55bc
edmilson-dk <droidvidaboa@gmail.com>
2021-04-30 14:07:09 -0300

    style: created theme colors variables

---
+++ src/styles/themes/colors.ts
@@ -0,0 +1,32 @@
+export const light = {
+  "bg-primary": "rgba(250, 250, 250, 0.97)",
+  "bg-secondary": "#F9F9F9",
+  "text": "#585757",
+};
+
+export const dark = {
+  "bg-primary": "#110F0F",
+  "bg-secondary": "#2B2A2A",
+  "text": "#E5E5E5",
+}
+
+export const defaultColors = {
+  "pink-400": "#F26391",
+  "pink-300": "rgba(252, 102, 150, 1)",
+  "pink-200": "rgba(255, 103, 152, 0.98)",
+  "pink-100": "rgba(255, 103, 152, 0.78)",
+  "violet-400": "#856FDE",
+  "violet-300": "#8E76EC",
+  "violet-200": "rgba(152, 126, 252, 0.98)",
+  "violet-100": "rgba(149, 124, 247, 0.85)",
+  "orange-400": "#F09730",
+  "orange-300": "#FCA138",
+  "orange-200": "rgba(252, 161, 56, 0.92)",
+  "orange-100": "rgba(255, 163, 57, 0.84)",
+  "green-400": "#2AB582",
+  "green-300": "#26BF87",
+  "green-200": "rgba(37, 196, 138, 0.9)",
+  "green-100": "rgba(33, 207, 143, 0.78)",
+  "red-200": "#F03C47",
+  "red-100": "rgba(240, 60, 71, 0.96)",
+}
