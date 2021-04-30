8dc0b60a32c923a716a4dba74d05fdeb5ba58131
edmilson-dk <droidvidaboa@gmail.com>
2021-04-30 18:31:30 -0300

    feat: added Theme component provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,13 @@
-import { AppProps } from 'next/app';
+import { AppProps } from "next/app";
 
-import GlobalStyle from 'src/styles/globalStyles';
+import GlobalStyle from "src/styles/globalStyles";
+import { Theme } from "src/styles/themes";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <Theme>
       <GlobalStyle />
       <Component {...pageProps} />
-    </>
-  )
+    </Theme>
+  );
 }
