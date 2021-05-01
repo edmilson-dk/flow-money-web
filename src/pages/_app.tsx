9c1517e260d4f89e5b98838337f7aa8158c87f81
edmilson-dk <droidvidaboa@gmail.com>
2021-05-01 14:50:30 -0300

    feat: created toggleTheme function

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,26 @@
 import { AppProps } from "next/app";
+import { ThemeProvider } from "styled-components";
+
+import { useCustomTheme } from "src/hooks/useCustomTheme";
 
 import GlobalStyle from "src/styles/globalStyles";
-import { Theme } from "src/styles/themes";
+import { darkTheme, defaultColors, lightTheme } from "src/styles/themes/colors";
+import { Header } from "src/components/generic/Header";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const [ theme, setTheme ] = useCustomTheme("@theme", lightTheme);
+
+  function toggleTheme() {
+    setTheme(theme.title === "dark" 
+      ? { ...lightTheme, ...defaultColors } 
+      : { ...darkTheme, ...defaultColors }
+    );
+  }
+
   return (
-    <Theme>
+    <ThemeProvider theme={theme}>
       <GlobalStyle />
       <Component {...pageProps} />
-    </Theme>
+    </ThemeProvider>
   );
 }
