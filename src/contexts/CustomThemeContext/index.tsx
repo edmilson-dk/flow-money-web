7677b7c9a980223844913c8db913ee68eddecb43
edmilson-dk <droidvidaboa@gmail.com>
2021-05-01 15:16:56 -0300

    feat: created custom theme context

---
+++ src/contexts/CustomThemeContext/index.tsx
@@ -0,0 +1,30 @@
+import { createContext, Dispatch, ReactNode, SetStateAction } from "react";
+import { useCustomTheme } from "src/hooks/useCustomTheme";
+import { darkTheme, lightTheme, ThemeColorsType } from "src/styles/themes/colors";
+
+type CustomThemeContext = {
+  toggleTheme: () => void;
+}
+
+type CustomThemeContextProviderProps = {
+  children: ReactNode;
+  theme: ThemeColorsType;
+  setTheme: Dispatch<SetStateAction<ThemeColorsType>>;
+}
+
+export const CustomThemeContext = createContext({} as CustomThemeContext);
+
+export function CustomThemeContextProvider({ children, theme, setTheme }: CustomThemeContextProviderProps) {
+  function toggleTheme() {
+    setTheme(theme.title === "dark" 
+      ? lightTheme 
+      : darkTheme
+    );
+  }
+
+  return (
+    <CustomThemeContext.Provider value={{ toggleTheme }}>
+      { children }
+    </CustomThemeContext.Provider>
+  );
+}
