043a3ff023ac29f5b09b1ea7d085283168c63f30
edmilson-dk <droidvidaboa@gmail.com>
2021-05-02 10:27:42 -0300

    feat: created banner component

---
+++ src/components/Home/Banner/index.tsx
@@ -0,0 +1,27 @@
+import { Container } from "src/styles/components/Container";
+import { BannerContainer, BannerWrapper } from "src/styles/components/Home/Banner";
+import { TitlePrimary } from "src/styles/components/titles";
+import { Button } from "../generic/Button";
+
+export function Banner() {
+  return (
+    <BannerWrapper>
+      <Container>
+        <BannerContainer>
+          <article>
+            <TitlePrimary>Gerencie suas finanças, simples e fácil</TitlePrimary>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer.</p>
+            <Button 
+              isRouterButton={true} 
+              routerButton="/user/register" 
+              text="Começar"
+            />
+          </article>
+          <div>
+            <img src="/png/bg-banner.png" alt="Flow money" />
+          </div>
+        </BannerContainer>
+      </Container>
+    </BannerWrapper>
+  );
+}
