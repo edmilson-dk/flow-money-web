403fe397a12a012b01a6bd92827a39f34ae5c066
edmilson-dk <droidvidaboa@gmail.com>
2021-05-02 10:30:17 -0300

    style: created generic switch theme button styles

---
+++ src/styles/components/generic/SwitchThemeButton/index.ts
@@ -0,0 +1,30 @@
+import styled, { keyframes } from "styled-components";
+
+const switchAnimate = keyframes`
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+`;
+
+export const SwitchThemeButtonWrapper = styled.button`
+    background: none;
+    margin-right: 2rem;
+    font-size: 0;
+    width: 60px;
+    height: 60px;
+    transform: rotate(0deg);
+    animation: ${switchAnimate} 3s ease-in-out infinite;
+
+    > span {
+      width: 100%;
+      height: 100%;
+
+      > img {
+        max-width: 100%;
+        max-height: 100%;
+      }
+    }
+`;
