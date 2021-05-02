d1c9bb6aad679814775d6fff3b62bfdf32a83d08
edmilson-dk <droidvidaboa@gmail.com>
2021-05-02 10:30:53 -0300

    refactor: removed nav button styles

--- src/styles/components/Home/generic/Header/index.ts
+++ src/styles/components/Home/generic/Header/index.ts
@@ -1,17 +1,8 @@
-import styled, { keyframes } from "styled-components";
-
-const switchAnimate = keyframes`
-  0% {
-    transform: rotate(0deg);
-  }
-  100% {
-    transform: rotate(360deg);
-  }
-`;
+import styled from "styled-components";
 
 export const HeaderWrapper = styled.header`
   height: 80px;
-  width: 100vw;
+  width: 100%;
 
   display: flex;
   justify-content: center;
@@ -47,39 +38,5 @@ export const HeaderContainer = styled.div`
     display: flex;
     justify-content: space-between;
     align-items: center;
-  }
-
-  > nav button {
-    background: none;
-    margin-right: 2rem;
-    font-size: 0;
-    width: 35px;
-    height: 35px;
-    transform: rotate(0deg);
-    animation: ${switchAnimate} 3s ease-in-out infinite;
-
-    > span {
-      width: 100%;
-      height: 100%;
-
-      > img {
-        max-width: 100%;
-        max-height: 100%;
-      }
-    }
-  }
-
-  > nav a {
-    width: 100px;
-    display: block;
-    padding: 1rem 0.5rem;
-
-    background-color: ${({ theme }) => theme.colors["pink-300"]};
-    border-radius: 20px;
-
-    font-size: 18px;
-    font-weight: 600;
-    text-align: center;
-    color: ${({ theme }) => theme.colors.white};
   }
 `;
