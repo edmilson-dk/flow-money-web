01d772130a5c7307fedebc1642eb1279597211d1
edmilson-dk <droidvidaboa@gmail.com>
2021-05-03 18:59:13 -0300

    style: created title secondary

--- src/styles/components/titles/index.ts
+++ src/styles/components/titles/index.ts
@@ -12,3 +12,23 @@ export const TitlePrimary = styled.h1<TitleProps>`
   text-align: center;
   margin-bottom: 1.875rem;
 `;
+
+export const TitleSecondary = styled.h2<TitleProps>`
+  font-size: 3.125rem;
+  line-height: 3.125rem;
+  font-weight: 700;
+  color: ${({ color, theme }) => color ?? theme.colors["violet-300"]};
+  text-align: left;
+  margin-bottom: 1.875rem;
+  position: relative;
+
+  &::after {
+    content: "";
+    display: block;
+    position: absolute;
+    width: 60px;
+    height: 10px;
+    transform: skewX(-45deg);
+    background-color: ${({ theme }) => theme.colors["green-200"]};
+  }
+`;
