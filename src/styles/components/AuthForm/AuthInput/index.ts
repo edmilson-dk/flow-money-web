573d1354c8a30a1f7b9be1c552d3f598f6f54149
edmilson-dk <droidvidaboa@gmail.com>
2021-05-04 13:21:49 -0300

    style: created auth input component styles

---
+++ src/styles/components/AuthForm/AuthInput/index.ts
@@ -0,0 +1,41 @@
+import styled from "styled-components";
+
+export const AuthInputWrappper = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: center;
+  
+  & + & {
+    margin-top: 15px;
+  }
+
+  > label {
+    font-size: 1.25rem;
+    font-weight: 500;
+    color: ${({ theme }) => theme.colors.text};
+    text-align: left;
+    margin-bottom: 5px;
+  }
+
+  > input {
+    width: 100%;
+    outline: none;
+    padding: 0 0.875rem;
+    height: 50px;
+    border-radius: 20px;
+    border: 2px solid ${({ theme }) => theme.colors["green-100"]};
+
+    font-size: 1.125rem;
+    font-weight: 400;
+    color: ${({ theme }) => theme.colors.text};
+    background-color: transparent;
+
+    transition: border-radius .4s ease-in-out;
+
+    &:focus {
+      border-color: ${({ theme }) => theme.colors["violet-100"]};
+    }
+  }
+`;
