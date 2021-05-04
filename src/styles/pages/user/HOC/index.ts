b870aef7a245d7c6213ae7d6f110eb3e1cd08440
edmilson-dk <droidvidaboa@gmail.com>
2021-05-04 13:50:27 -0300

    feat: created user HOC page styles

---
+++ src/styles/pages/user/HOC/index.ts
@@ -0,0 +1,28 @@
+import styled from "styled-components";
+
+export const UserFormAuthHocWrapper = styled.section`
+  width: 100%;
+  min-height: calc(100vh - 80px);
+  padding: 3.75rem 0;
+  background-color: ${({ theme }) => theme.colors.bgPrimary};
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  @media screen and (max-width: 980px) {
+    height: 100%;
+  }
+`;
+
+export const UserFormAuthHocContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  > main {
+    width: 100%;
+  }
+`;
