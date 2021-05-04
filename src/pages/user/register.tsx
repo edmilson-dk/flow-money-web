3ce415a9b3a828dc3c3dd86298031b748b6dbb95
edmilson-dk <droidvidaboa@gmail.com>
2021-05-04 13:51:42 -0300

    feat: created initial register page structure

---
+++ src/pages/user/register.tsx
@@ -0,0 +1,40 @@
+import { AuthForm } from "src/components/AuthForm";
+import { AuthButton } from "src/components/AuthForm/AuthButton";
+import { AuthInput } from "src/components/AuthForm/AuthInput";
+import { TitleSecondary } from "src/styles/components/titles";
+import { UserFormAuthHoc } from "./_HOC";
+
+function Register() {
+  return (
+    <main>
+      <TitleSecondary>Seja bem vindo! </TitleSecondary>
+      <AuthForm onSubmit={() => console.log("form")}>
+        <AuthInput 
+          type="name"
+          onChange={(e) => console.log(e.target)}
+          label="Nome"
+          name="name"
+        />
+        <AuthInput 
+          type="email"
+          onChange={(e) => console.log(e.target)}
+          label="Email"
+          name="email"
+        />
+        <AuthInput 
+          type="password"
+          onChange={(e) => console.log(e.target)}
+          label="Password"
+          name="password"
+        />
+        <AuthButton 
+          type="button"
+          onClick={() => console.log("clicked")}
+          text="Entrar"
+        />
+      </AuthForm>
+    </main>
+  );
+}
+
+export default UserFormAuthHoc(Register);
