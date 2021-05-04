ccc156b2a9d13268c50593167d30b877b0b6a9a7
edmilson-dk <droidvidaboa@gmail.com>
2021-05-04 20:49:15 -0300

    feat: created authToken service

---
+++ src/services/authToken/index.ts
@@ -0,0 +1,45 @@
+import Cookies from "js-cookie";
+import jwtDecode from "jwt-decode";
+import Router from "next/router";
+
+type DecodedTokenType = {
+  email: string;
+  exp: number;
+}
+
+export const TOKEN_STORED_KEY = "flowMoneyUserTokenKey";
+
+export class AuthToken {
+  readonly decodedToken: DecodedTokenType;
+
+  constructor(readonly token: string) {
+    this.decodedToken = { email: "", exp: 0 };
+
+    try {
+      if (token) this.decodedToken = jwtDecode(token);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  get authorizationString() {
+    return `Bearer ${this.token}`;
+  }
+
+  get expiresAt(): Date {
+    return new Date(this.decodedToken.exp * 1000);
+  }
+
+  get isExpired(): boolean {
+    return new Date() > this.expiresAt;
+  }
+
+  get isValid(): boolean {
+    return !this.isExpired;
+  }
+
+  static async storeToken(token: string) {
+    Cookies.set(TOKEN_STORED_KEY, token);
+    await Router.push("/dashboard");
+  }
+}
