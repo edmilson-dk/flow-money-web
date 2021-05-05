c6dc944d4f0e28489118d4a89b5c0ef1669ce4c6
edmilson-dk <droidvidaboa@gmail.com>
2021-05-04 21:00:26 -0300

    feat: added sign up user api call

--- src/pages/user/register.tsx
+++ src/pages/user/register.tsx
@@ -1,34 +1,56 @@
+import { useContext, useState } from "react";
+import { toast } from "react-toastify";
+
 import { AuthForm } from "src/components/AuthForm";
 import { AuthButton } from "src/components/AuthForm/AuthButton";
 import { AuthInput } from "src/components/AuthForm/AuthInput";
+import { AuthUserContext } from "src/contexts/AuthUserContext";
 import { TitleSecondary } from "src/styles/components/titles";
 import { UserFormAuthHoc } from "./_HOC";
 
 function Register() {
+  const { signUp } = useContext(AuthUserContext);
+
+  const [ name, setName ] = useState(null);
+  const [ email, setEmail ] = useState(null);
+  const [ password, setPassword ] = useState(null);
+
+  async function handlerSubmit(e) {
+    e.preventDefault();
+
+    const data = {
+      name,
+      email,
+      password,
+    };
+  
+    await signUp(data);
+  } 
+
   return (
     <main>
       <TitleSecondary>Seja bem vindo! </TitleSecondary>
-      <AuthForm onSubmit={() => console.log("form")}>
+      <AuthForm onSubmit={handlerSubmit}>
         <AuthInput 
           type="name"
-          onChange={(e) => console.log(e.target)}
+          onChange={(e) => setName(e.target.value)}
           label="Nome"
           name="name"
         />
         <AuthInput 
           type="email"
-          onChange={(e) => console.log(e.target)}
+          onChange={(e) => setEmail(e.target.value)}
           label="Email"
           name="email"
         />
         <AuthInput 
           type="password"
-          onChange={(e) => console.log(e.target)}
+          onChange={(e) => setPassword(e.target.value)}
           label="Password"
           name="password"
         />
         <AuthButton 
-          type="button"
+          type="submit"
           onClick={() => console.log("clicked")}
           text="Entrar"
         />
