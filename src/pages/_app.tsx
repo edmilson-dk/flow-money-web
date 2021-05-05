913d7c7f2a7af2081412d296e13e1f385e781bf9
edmilson-dk <droidvidaboa@gmail.com>
2021-05-04 21:01:32 -0300

    feat: added auth user context provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,19 +2,23 @@ import { AppProps } from "next/app";
 import { ThemeProvider } from "styled-components";
 
 import GlobalStyle from "src/styles/globalStyles";
+
 import { CustomThemeContextProvider } from "src/contexts/CustomThemeContext";
 import { useCustomTheme } from "src/hooks/useCustomTheme";
 import { lightTheme } from "src/styles/themes/colors";
+import { AuthUserContextProvider } from "src/contexts/AuthUserContext";
 
 export default function App({ Component, pageProps }: AppProps) {
-  const [ theme, setTheme ] = useCustomTheme("@theme", lightTheme);
+  const [ theme, setTheme ] = useCustomTheme("flowMoneyTheme", lightTheme);
 
   return (
-    <CustomThemeContextProvider theme={theme} setTheme={setTheme}>
-      <ThemeProvider theme={theme}>
-        <GlobalStyle />
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </CustomThemeContextProvider>
+    <AuthUserContextProvider>
+      <CustomThemeContextProvider theme={theme} setTheme={setTheme}>
+        <ThemeProvider theme={theme}>
+          <GlobalStyle />
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </CustomThemeContextProvider>
+    </AuthUserContextProvider>
   );
 }
