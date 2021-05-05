3f34278722478173a4747c3b9645940cd3aac7ec
edmilson-dk <droidvidaboa@gmail.com>
2021-05-04 21:00:10 -0300

    feat: added sign in user api call

--- src/pages/user/login.tsx
+++ src/pages/user/login.tsx
@@ -1,28 +1,46 @@
+import { useContext, useState } from "react";
 import { AuthForm } from "src/components/AuthForm";
 import { AuthButton } from "src/components/AuthForm/AuthButton";
 import { AuthInput } from "src/components/AuthForm/AuthInput";
+import { AuthUserContext } from "src/contexts/AuthUserContext";
 import { TitleSecondary } from "src/styles/components/titles";
 import { UserFormAuthHoc } from "./_HOC";
 
 function Login() {
+  const { signIn } = useContext(AuthUserContext);
+
+  const [ email, setEmail ] = useState(null);
+  const [ password, setPassword ] = useState(null);
+
+  async function handlerSubmit(e) {
+    e.preventDefault();
+
+    const data = {
+      email,
+      password,
+    };
+
+    await signIn(data);
+  } 
+
   return (
     <main>
       <TitleSecondary>Bem vindo de volta!</TitleSecondary>
-      <AuthForm onSubmit={() => console.log("form")}>
+      <AuthForm onSubmit={handlerSubmit}>
         <AuthInput 
           type="email"
-          onChange={(e) => console.log(e.target)}
+          onChange={(e) => setEmail(e.target.value)}
           label="Email"
           name="email"
         />
         <AuthInput 
           type="password"
-          onChange={(e) => console.log(e.target)}
+          onChange={(e) => setPassword(e.target.value)}
           label="Password"
           name="password"
         />
         <AuthButton 
-          type="button"
+          type="submit"
           onClick={() => console.log("clicked")}
           text="Entrar"
         />
