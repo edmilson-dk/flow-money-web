f1e4b31a5448040308bd0b99eacb1691587fac81
edmilson-dk <droidvidaboa@gmail.com>
2021-05-06 15:18:03 -0300

    feat: created user form auth hoc component

---
+++ src/hoc-components/UserFormAuthHoc/index.tsx
@@ -0,0 +1,28 @@
+import { useContext } from "react";
+import { Header } from "src/components/Home/generic/Header"
+import { CustomThemeContext } from "src/contexts/CustomThemeContext";
+import { Container } from "src/styles/components/Container";
+import { UserFormAuthHocContainer, UserFormAuthHocWrapper } from "src/styles/pages/user/HOC";
+
+export function UserFormAuthHoc(WrappedComponent: any) {
+  return () => {
+    const { toggleTheme } = useContext(CustomThemeContext);
+
+    return (
+      <>
+        <Header 
+          toggleTheme={toggleTheme}
+          buttonText="Home"
+          buttonRouter="/"
+        />
+        <UserFormAuthHocWrapper>
+          <Container>
+            <UserFormAuthHocContainer>
+              <WrappedComponent />
+            </UserFormAuthHocContainer>
+          </Container>
+        </UserFormAuthHocWrapper>
+      </>
+    );
+  };
+}
