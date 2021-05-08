9f72ecd0833da6fb58762a1fe199cf0238e90157
edmilson-dk <droidvidaboa@gmail.com>
2021-05-08 09:31:57 -0300

    style: removed strong display none

--- src/styles/components/Dashboard/DashboardNavBar/index.ts
+++ src/styles/components/Dashboard/DashboardNavBar/index.ts
@@ -20,5 +20,5 @@ export const DashboardNavBarContent = styled.main`
   width: calc(100% - 280px);
   overflow-y: scroll;
   background-color: ${({ theme }) => theme.colors.white};
-  padding: 60px;
+  padding: 0 60px;
 `;
