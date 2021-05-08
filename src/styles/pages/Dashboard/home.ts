d29c1246cf7071e032d163831a7e9f60f917d2ec
edmilson-dk <droidvidaboa@gmail.com>
2021-05-08 17:11:31 -0300

    style: created dashboard-content styles

--- src/styles/pages/Dashboard/home.ts
+++ src/styles/pages/Dashboard/home.ts
@@ -1,12 +1,17 @@
 import styled from "styled-components";
 
-export const DashBoardHeader = styled.header`
+export const DashboardHeader = styled.header`
   width: 100%;
   height: 180px;
   background-color: ${({ theme }) => theme.colors["violet-400"]};
   position: relative;
-  display: grid;
-  grid-template-columns: repeat(3,1fr);
-  column-gap: 1.875rem;
-  padding: 0 60px;
+
+  > div {
+    display: grid;
+    grid-template-columns: repeat(3,1fr);
+    column-gap: 1.875rem;
+  }
+`;
+
+export const DashboardContent = styled.section`
 `;
