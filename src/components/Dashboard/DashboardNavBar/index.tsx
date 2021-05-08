2e15ecb1c6336c16b927384541a34bb5a3679602
edmilson-dk <droidvidaboa@gmail.com>
2021-05-08 08:38:52 -0300

    feat: added dashboard nav bar navigator component

--- src/components/Dashboard/DashboardNavBar/index.tsx
+++ src/components/Dashboard/DashboardNavBar/index.tsx
@@ -1,20 +1,36 @@
+import { useRouter } from "next/router";
 import { ReactNode } from "react";
+import { FiHome, FiPlus, FiSettings } from "react-icons/fi";
 
 import { 
   DashboardNavBarContent, 
   DashboardNavBarMenu, 
   DashboardNavBarWrapper
 } from "src/styles/components/Dashboard/DashboardNavBar";
+import { DashboardNavBarNavigator } from "../DashboardNavBarNavigator";
 
 type DashboardNavBarProps = {
   children: ReactNode;
 };
 
 export function DashboardNavBar({ children }: DashboardNavBarProps) {
+  const navigator = [
+    {
+      router: "/dashboard",
+      icon: <FiHome size="100%"/>,
+      text: "Home"
+    },
+    {
+      router: "/dashboard/create",
+      icon: <FiPlus size="100%"/>,
+      text: "Adicionar"
+    },
+  ];
+
   return (
     <DashboardNavBarWrapper>
       <DashboardNavBarMenu>
-        <h3>Flow Money</h3>
+        <DashboardNavBarNavigator data={navigator}/>
       </DashboardNavBarMenu>
       <DashboardNavBarContent>
         { children }
