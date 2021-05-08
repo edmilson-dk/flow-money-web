cd06073c324daac2f0c22be0b5443eac7ad60180
edmilson-dk <droidvidaboa@gmail.com>
2021-05-08 09:31:31 -0300

    style: created dashboard home header styles

---
+++ src/styles/pages/Dashboard/home.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const DashBoardHeader = styled.header`
+  width: 100%;
+  height: 180px;
+  position: relative;
+  display: grid;
+  grid-template-columns: repeat(3,1fr);
+  column-gap: 1.875rem;
+  padding: 0 1.875rem;
+`;
