1a5e3b03d7527a4df60e8203765673b97844b6ad
edmilson-dk <droidvidaboa@gmail.com>
2021-05-08 09:52:36 -0300

    style: added background

--- src/styles/pages/Dashboard/home.ts
+++ src/styles/pages/Dashboard/home.ts
@@ -3,6 +3,7 @@ import styled from "styled-components";
 export const DashBoardHeader = styled.header`
   width: 100%;
   height: 180px;
+  background-color: ${({ theme }) => theme.colors["violet-400"]};
   position: relative;
   display: grid;
   grid-template-columns: repeat(3,1fr);
