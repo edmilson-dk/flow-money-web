9fa141bd10c4ee8d369633752be77c6d853bb709
edmilson-dk <droidvidaboa@gmail.com>
2021-05-08 08:39:22 -0300

    style: added new background color

--- src/styles/components/Dashboard/DashboardNavBar/index.ts
+++ src/styles/components/Dashboard/DashboardNavBar/index.ts
@@ -7,21 +7,13 @@ export const DashboardNavBarWrapper = styled.section`
   display: flex;
 `;
 
-export const DashboardNavBarMenu = styled.nav`
+export const DashboardNavBarMenu = styled.aside`
   width: 280px;
-  background-color: ${({ theme }) => theme.colors["green-400"]};
+  background-color: ${({ theme }) => theme.colors["violet-400"]};
   min-height: 100vh;
   z-index: 9999;
   padding: 20px 0;
-
-  > h3 {
-    font-size: 1.5rem;
-    text-align: center;
-    font-weight: 600;
-    color: ${({ theme }) => theme.colors.white};
-    border-bottom: 1px solid ${({ theme }) => theme.colors.white};
-    padding-bottom: 20px;
-  }
+  position: relative;
 `;
 
 export const DashboardNavBarContent = styled.main`
