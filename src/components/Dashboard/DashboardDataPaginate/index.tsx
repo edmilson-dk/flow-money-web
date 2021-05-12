18c2872356d80492af5ad97553fa76e8caf4829f
edmilson-dk <droidvidaboa@gmail.com>
2021-05-12 08:26:22 -0300

    feat: added dashboard data paginate buttons disabled property

--- src/components/Dashboard/DashboardDataPaginate/index.tsx
+++ src/components/Dashboard/DashboardDataPaginate/index.tsx
@@ -3,20 +3,20 @@ import { FiChevronLeft, FiChevronRight } from "react-icons/fi";
 import { DashboardDataPaginateWrapper } from "src/styles/components/Dashboard/DashboardDataPaginate";
 import { DashboardDataPaginateProps } from "./types";
 
-export const DashboardDataPaginate = React.memo(({ nextCbFetch, prevCbFetch }: DashboardDataPaginateProps) => {
-  console.log("chan")
+export const DashboardDataPaginate = ({ nextCbFetch, prevCbFetch, nextDisabled, prevDisabled }: DashboardDataPaginateProps) => {
+
   return (
     <DashboardDataPaginateWrapper>
-      <button type="button" onClick={prevCbFetch}>
+      <button type="button" onClick={prevCbFetch} disabled={prevDisabled}>
         <span>
           <FiChevronLeft size="100%"/>
         </span>
       </button>
-      <button type="button" onClick={nextCbFetch}>
+      <button type="button" onClick={nextCbFetch} disabled={nextDisabled}>
         <span>
           <FiChevronRight size="100%"/>
         </span>
       </button>
     </DashboardDataPaginateWrapper>
   );
-});
+};
