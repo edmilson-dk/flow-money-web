36dffd3e1642469519b71955c5e69724595e3c3f
edmilson-dk <droidvidaboa@gmail.com>
2021-05-15 20:10:06 -0300

    style: created auth sign in or sign up link action styles

--- src/styles/components/AuthForm/index.ts
+++ src/styles/components/AuthForm/index.ts
@@ -14,4 +14,13 @@ export const AuthFormWrapper = styled.form`
 
   background-color: ${({ theme }) => theme.colors.bgSecondary};
   box-shadow: 0 0 4px 3px rgba(0,0,0,0.090);
+
+  > a#authSignInOrSignUp {
+    color: ${({ theme }) => theme.colors["orange-400"]};
+    font-weight: 500;
+    text-align: left;
+    padding: 10px;
+    margin-top: 10px;
+    align-self: flex-start;
+  }
 `;
