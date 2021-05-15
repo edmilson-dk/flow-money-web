57e9ae06b368e13214312c053812973ae9cd0c8d
edmilson-dk <droidvidaboa@gmail.com>
2021-05-15 19:33:21 -0300

    style: created logout button styles

--- src/styles/components/Dashboard/DashboardNavBarNavigator/index.ts
+++ src/styles/components/Dashboard/DashboardNavBarNavigator/index.ts
@@ -2,9 +2,60 @@ import styled from "styled-components";
 
 export const DashboardNavBarNavigatorWrapper = styled.nav`
   width: 100%;
+  position: relative;
   height: 100%;
   padding-top: 60px;
   display: flex;
   flex-direction: column;
   align-items: center;
+
+  > button#logout {
+    position: absolute;
+    bottom: 0px;
+
+    transition: all .3s ease-in-out;
+    width: 70%;
+    background-color: transparent;
+    border-radius: 10px;
+
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0.5rem 1rem;
+    border: 3px solid ${({ theme }) => theme.colors["violet-200"]};
+
+    & + & {
+      margin-top: 30px;
+    }
+
+    > strong {
+      font-size: 1.125rem;
+      font-weight: 600;
+      color: ${({ theme }) => theme.colors.white};
+    }
+
+    > span {
+      width: 30px;
+
+      > svg {
+        width: 30px;
+        stroke: ${({ theme }) => theme.colors.white};
+      }
+    }
+  }
+
+  > button,
+  > button#logout {
+    &:hover {
+      background-color: ${({ theme }) => theme.colors.white};
+
+      > strong {
+        color: ${({ theme }) => theme.colors["green-300"]};
+      }
+
+      > span svg {
+        stroke: ${({ theme }) => theme.colors["green-300"]}; 
+      }
+    }
+  }
 `;
