4565b016894656340dc9eee03b3450ae50288985
edmilson-dk <droidvidaboa@gmail.com>
2021-05-15 21:40:51 -0300

    feat: created title third styled component

--- src/styles/components/Dashboard/DashboardTitle/index.ts
+++ src/styles/components/Dashboard/DashboardTitle/index.ts
@@ -1,11 +1,23 @@
 import styled from "styled-components";
 
+import { TitleThirdProps } from "./types";
+
 export const TitlePrimary = styled.h2`
   font-size: 3.125rem;
   line-height: 3.125rem;
   font-weight: 700;
   color: ${({ color, theme }) => color ?? theme.colors["gray-200"]};
   text-align: left;
-  margin-bottom: 1.875rem;
+  margin-bottom: 30px;
+  position: relative;
+`;
+
+export const TitleThird = styled.h3<TitleThirdProps>`
+  font-size: 1.875rem;
+  line-height: 1.875rem;
+  font-weight: 700;
+  color: ${({ color, theme }) => color ?? theme.colors["gray-200"]};
+  text-align: ${({ align }) => align};
+  margin-bottom: 20px;
   position: relative;
 `;
